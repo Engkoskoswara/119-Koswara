8c996e9a2ee464440eb52ffefe8bcea1213413b7
Engkoskoswara <56513831+Engkoskoswara@users.noreply.github.com>
2024-05-10 11:50:46 +0700

    perbarui

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Beranda</title>
+    </head>
+    <body>
+        <h1>ini halaman beranda</h1>
+        <a href="{{userss.detail}}">detail</a>
+    </body>
+</html>
